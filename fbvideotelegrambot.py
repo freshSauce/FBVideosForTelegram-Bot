26430eda2557810993cf7c241c6038c35547c675
MrH0wl <wazudream@gmail.com>
2020-10-13 02:43:03 -0600

    Send video directly instead of source link

--- fbvideotelegrambot.py
+++ fbvideotelegrambot.py
@@ -2,52 +2,33 @@ import requests as r, bs4, urllib.parse as u, telebot
 from time import sleep
 
 def HTMLParser(url):
-    # print(url)
-    url = r.get(url)
-    UrlHTML = bs4.BeautifulSoup(url.text, 'lxml')
-    videoSource = UrlHTML.findAll('a', href=True)
-    videoTag = [i for i in videoSource if 'mp4' in str(i)][0]
-    videoTag = str(videoTag).split(';')[0]
-    videoUrl = videoTag.split('src=')[1]
-    return u.unquote(str(videoUrl))
+	UrlHTML = bs4.BeautifulSoup(r.get(url).text, 'lxml')
+	videoUrl = str([i for i in UrlHTML.findAll('a', href=True) if 'mp4' in str(i)][0]).split(';')[0].split('src=')[1]
+	return u.unquote(str(videoUrl))
 
 def VideoUrl(url):
-    try:
-        if 'www' in url:
-            url = url.replace('www', 'm')
-            # print(url)
-        elif '//f' in url:
-            url = url.replace('//f', '//m.f')
-            # print(url)
-        elif 'm.' in url:
-            pass
-        else:
-            return 'Your link doesn\'t work'
-        return HTMLParser(url)
-    except:
-        return 'An error has occured, please contact the bot creator.'
+	try:
+		url = 'https://m.facebook.com/' + url.split('.com', 1)[1] if 'facebook' in url else 'https://m.facebook.com/' + url
+		return HTMLParser(url) #return (f'Your Video URL is: {HTMLParser(url)}')
+	except Exception as e:
+		return 'An error has occurred, please contact the script creator.'
 
 if __name__ == "__main__":
 
 
-    bot_token = '<BOT_TOKEN>'
-    bot = telebot.TeleBot(token=bot_token)
+	bot_token = '<BOT_TOKEN>'
+	bot = telebot.TeleBot(token=bot_token)
 
 
-    @bot.message_handler(commands=['start'])
-    def send_welcome(message):
-        bot.reply_to(message, 'TEST MESSAGE')
-
-    @bot.message_handler(func=lambda msg: msg.text is not None and '@BotName' in msg.text)
-    def at_answer(message):
-        fburl = message.text.split()[1]
-        if 'http' not in fburl:
-            fburl ='http://'+fburl
-        bot.reply_to(message, VideoUrl(fburl))
-
-    while True:
-        try:
-            bot.polling()
-        except Exception:
-            sleep(15)
-
+	@bot.message_handler(commands=['start'])
+	def send_welcome(message):
+		bot.reply_to(message, 'Hello Test')
+	@bot.message_handler(func=lambda msg: msg.text is not None and '@BotName' in msg.text)
+	def at_answer(message):
+		fburl = message.text.split()[1]
+		bot.send_video(message.chat.id, VideoUrl(fburl), supports_streaming=True, reply_to_message_id=message.message_id)
+	while True:
+		try:
+			bot.polling()
+		except Exception:
+			sleep(15)
